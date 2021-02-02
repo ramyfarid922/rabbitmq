73b274a49850a353184a96ebb86d9a4a1a617a53
Ramy <ramyfarid922@gmail.com>
2021-02-02 21:03:29 +0200

    Added the publisher client code

--- publisher.js
+++ publisher.js
@@ -1,7 +1,22 @@
+// Require the nodejs amqp protocol client
 const amqp = require("amqplib");
 
+// Prepare the message to be published
+const msg = { number: 19 };
+
+// Start the connection with the RabbitMQ server (spun by docker)
+connect();
+
+// Define the connect function
 async function connect() {
   try {
     const connection = await amqp.connect("amqp://localhost:5672");
-  } catch (ex) {}
+
+    const channel = await connection.createChannel();
+    const result = await channel.assertQueue("jobs");
+    channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+    console.log(`Job send successfully ${msg.number}`);
+  } catch (ex) {
+    console.error(ex);
+  }
 }
